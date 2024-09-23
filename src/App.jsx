e731ca970589fc65d63560c455e935875878e31f
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-09-23 05:43:28 +0100

    Add components needed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css'
+import LoginForm from './components/Auth/LoginForm'
 
 function App() {
-  
-
   return (
     <>
-      
+    <LoginForm />
     </>
   )
 }
---
+++ src/components/Auth/LoginForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoginForm = () => {
+  return (
+    <>
+     <div>LoginForm</div>
+     <h1>tyyy</h1>
+    </>
+   
+  )
+}
+
+export default LoginForm
