d21b6426ddc7a6600cab37ade591e9b5f407013f
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-09-24 03:16:06 +0100

    Routingimplemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import './App.css'
-import LoginForm from './components/Auth/LoginForm'
+//Imports
+import './App.css';
+import { ToastContainer } from 'react-toastify';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginForm from './components/Auth/LoginForm';
+import RegisterForm from './components/Auth/RegisterForm';
+import ForgotPasswordForm from './components/Auth/ForgotPasswordForm';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <>
-    <LoginForm />
-    </>
-  )
+    <Router>
+      <div> 
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/home" element={<Home/>} />
+          <Route path="/forgot" element={<ForgotPasswordForm/>} />
+        </Routes>
+      </div>
+      <ToastContainer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
